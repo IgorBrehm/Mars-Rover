c22952bf5893747480257c06e6de6c7dc72c0ef4
Parmeggiani <vini.vpv@gmail.com>
2017-08-24 19:30:39 -0300

    Update ReadFile.java

--- ReadFile.java
+++ ReadFile.java
@@ -9,31 +9,31 @@ import java.io.*;
 public void readFile(File file){
 	try{
 		FileReader arq = new FileReader(file);
-        BufferedReader readF = new BufferedReader(arq);
+        	BufferedReader readF = new BufferedReader(arq);
            
-        String lineOne = readF.readLine();//Primeira linha do arquivo
-        String[] tabuleiro = lineOne.split(" ");
-        int maxX = Integer.parseInt(tabuleiro[0]);
-        int maxY = Integer.parseInt(tabuleiro[1]);
+        	String lineOne = readF.readLine();//Primeira linha do arquivo
+        	String[] tabuleiro = lineOne.split(" ");
+        	int maxX = Integer.parseInt(tabuleiro[0]);
+        	int maxY = Integer.parseInt(tabuleiro[1]);
         
 		String line = readF.readLine();//Segunda linha do arquivo
 		while(line != null){
-            String[] positions = line.split(" ");
-            int x = Integer.parseInt(positions[0]);
-            int y = Integer.parseInt(positions[1]);
-            char[] direction = positions[2].toCharArray();
-            Rover rover = new Rover(x,y,direction[0]);
+            		String[] positions = line.split(" ");
+            		int x = Integer.parseInt(positions[0]);
+            		int y = Integer.parseInt(positions[1]);
+            		char[] direction = positions[2].toCharArray();
+            		Rover rover = new Rover(x,y,direction[0]);
+            		
+			line = readF.readLine();
+            		String result = rover.move(rover, maxY, maxX, line);
+            		System.out.println(result);
             
 			line = readF.readLine();
-            String result = rover.move(rover, maxY, maxX, line);
-            System.out.println(result);
-            
-			line = readF.readLine();
-        }
-        arq.close();
-    }
-    catch (IOException e) {
-        System.err.printf("Erro na abertura do arquivo: %s.\n",
-        e.getMessage());
-    }
+        	}
+        	arq.close();
+    	}
+    	catch (IOException e) {
+        	System.err.printf("Erro na abertura do arquivo: %s.\n",
+        	e.getMessage());
+    	}
 }
